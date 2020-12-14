860c4e3bcbbba1e0d9678c59e3142debe6c09aee
Ignas Veleckis <ignas.veleckis@outlook.com>
2020-12-14 22:14:57 +0200

    xx

--- app.js
+++ app.js
@@ -1,150 +1,119 @@
-const board_dom = document.querySelector(".board");
-let node_selected = "start";
+const board_dom = document.querySelector('.board');
+let node_selected = 'start';
+let arrayCopy = [];
 
 const createInitialBoard = (rows, cols) => {
-  const board = [];
-  for (let y = 0; y < rows; y++) {
-    const row = [];
-    for (let x = 0; x < cols; x++) {
-      row.push({ x, y });
+    const board = [];
+    for (let y = 0; y < rows; y++) {
+        const row = [];
+        for (let x = 0; x < cols; x++) {
+            row.push({ x, y });
+        }
+        board.push(row);
     }
-    board.push(row);
-  }
-  generateBoardInDom(board);
-  document.querySelector(".board_title").innerHTML = `Board: ${cols} x ${rows}`;
+    generateBoardInDom(board);
+    document.querySelector(
+        '.board_title'
+    ).innerHTML = `Board: ${cols} x ${rows}`;
 };
 
 const generateBoardInDom = (array) => {
-  for (let y in array) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let x in array[y]) {
-      const col = document.createElement("div");
-      col.classList.add("col");
-      col.addEventListener("click", () => {
-        if (node_selected === "start") {
-          for (let a in board_dom.children) {
-            for (let z in board_dom.children[a].children) {
-              if (board_dom.children[a].children[z].innerHTML === "S") {
-                board_dom.children[a].children[z].innerHTML = "";
-              }
-            }
-          }
-          col.innerHTML = "S";
-          col.classList.add("green");
-        } else if (node_selected === "end") {
-          for (let a in board_dom.children) {
-            for (let z in board_dom.children[a].children) {
-              if (board_dom.children[a].children[z].innerHTML === "E") {
-                board_dom.children[a].children[z].innerHTML = "";
-              }
-            }
-          }
-          col.innerHTML = "E";
-          col.classList.add("red");
-        } else if (node_selected === "stop") {
-          let stopsCount = 0;
-          for (let a in board_dom.children) {
-            for (let z in board_dom.children[a].children) {
-              if (board_dom.children[a].children[z].innerHTML === "X") {
-                stopsCount++;
-              }
-            }
-          }
-          if (stopsCount < 3) {
-            col.innerHTML = "X";
-            col.classList.add("yellow");
-          }
+    arrayCopy = [...array];
+    for (let y in array) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let x in array[y]) {
+            const col = document.createElement('div');
+            col.classList.add('col');
+            col.addEventListener('click', () => {
+                if (node_selected === 'start') {
+                    for (let a in board_dom.children) {
+                        for (let z in board_dom.children[a].children) {
+                            if (
+                                board_dom.children[a].children[z].className &&
+                                board_dom.children[a].children[z].className ===
+                                    'col green'
+                            ) {
+                                board_dom.children[a].children[z].className =
+                                    'col';
+                            }
+                        }
+                    }
+                    col.classList.add('green');
+                } else if (node_selected === 'end') {
+                    for (let a in board_dom.children) {
+                        for (let z in board_dom.children[a].children) {
+                            if (
+                                board_dom.children[a].children[z].className &&
+                                board_dom.children[a].children[z].className ===
+                                    'col red'
+                            ) {
+                                board_dom.children[a].children[z].className =
+                                    'col';
+                            }
+                        }
+                    }
+                    col.classList.add('red');
+                }
+            });
+            row.appendChild(col);
         }
-      });
-      row.appendChild(col);
+        board_dom.appendChild(row);
     }
-    board_dom.appendChild(row);
-  }
 };
 
 const changeNode = (node) => {
-  node_selected = node;
+    node_selected = node;
 };
 
 const search = () => {
-  let start = "";
-  const stops = [];
-  let end = "";
+    let start = '';
+    let end = '';
 
-  for (let x in board_dom.children) {
-    for (let z in board_dom.children[x].children) {
-      if (board_dom.children[x].children[z].innerHTML === "S") {
-        start = `x${z}y${x}`;
-      } else if (board_dom.children[x].children[z].innerHTML === "X") {
-        stops.push(`x${z}y${x}`);
-      } else if (board_dom.children[x].children[z].innerHTML === "E") {
-        end = {
-          x: Number(z),
-          y: Number(x),
-        };
-      }
-    }
-  }
-
-  const allStartPoints = [];
-
-  for (let x in board_dom.children) {
-    for (let z in board_dom.children[x].children) {
-      if (board_dom.children[x].children[z].innerHTML === "S") {
-        const coordinatesPlusY = {
-          x: Number(z),
-          y: Number(x) - 1,
-        };
-        const coordinatesMinusY = {
-          x: Number(z),
-          y: Number(x) + 1,
-        };
-        const coordinatesMinusX = {
-          x: Number(z) - 1,
-          y: Number(x),
-        };
-        const coordinatesPlusX = {
-          x: Number(z) + 1,
-          y: Number(x),
-        };
-
-        allStartPoints.push(coordinatesMinusX);
-        allStartPoints.push(coordinatesPlusX);
-        allStartPoints.push(coordinatesMinusY);
-        allStartPoints.push(coordinatesPlusY);
-      }
-    }
-  }
-
-  for (let x in board_dom.children) {
-    for (let z in board_dom.children[x].children) {
-      for (let y in allStartPoints) {
-        if (
-          allStartPoints[y].x == z &&
-          allStartPoints[y].y == x &&
-          board_dom.children[x].children[z].innerHTML !== "E"
-        ) {
-          board_dom.children[x].children[z].classList.add("green");
-          board_dom.children[x].children[z].innerHTML = "S";
+    for (let y in board_dom.children) {
+        for (let x in board_dom.children[y].children) {
+            if (board_dom.children[y].children[x].className === 'col green') {
+                start = { x: Number(x), y: Number(y) };
+            } else if (
+                board_dom.children[y].children[x].className === 'col red'
+            ) {
+                end = { x: Number(x), y: Number(y) };
+            }
         }
-      }
     }
-  }
-  console.log(allStartPoints);
+
+    for (let y in arrayCopy) {
+        for (let x in arrayCopy[y]) {
+            if (
+                arrayCopy[y][x].x === start.x &&
+                arrayCopy[y][x].y === start.y
+            ) {
+                console.log('start');
+                console.log(start);
+            } else if (
+                arrayCopy[y][x].x === end.x &&
+                arrayCopy[y][x].y === end.y
+            ) {
+                console.log('end');
+                console.log(end);
+            }
+        }
+    }
+
+    //const searchInterval = setInterval(() => {}, 1000);
 };
 
 const startSearch = () => {
-  // return somethin
-  // then clear interval
-  // when end is found
-  /* setInterval(() => {
+    // return somethin
+    // then clear interval
+    // when end is found
+    /* setInterval(() => {
     search();
   }, 200); */
-  search();
+    search();
 };
 
-createInitialBoard(20, 20);
+createInitialBoard(5, 5);
 
 /* for (let x in board_dom.children) {
     let breakLoop = false;
